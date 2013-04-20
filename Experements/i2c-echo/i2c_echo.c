383551791e1833ddcd4fb85e62fbbfa51f21c6a8
Wil Selwood <wselwood@gmail.com>
2013-04-20 19:26:52 +0100

    inital commit

---
+++ Experements/i2c-echo/i2c_echo.c
@@ -0,0 +1,40 @@
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+#include <stdio.h>
+
+
+int main (int argc, char ** argv) {
+  int file;
+  int adapter_nr = 1; /* probably dynamically determined */
+  char filename[20];
+  
+  snprintf(filename, 19, "/dev/i2c-%d", adapter_nr);
+  file = open(filename, O_RDWR);
+  if (file < 0) {
+    /* ERROR HANDLING; you can check errno to see what went wrong */
+    printf("ARRRGGGGGGGG File open failed\n");
+    return(1);
+  }
+  
+  int addr = 0x40; /* The I2C address */
+
+  if (ioctl(file, I2C_SLAVE, addr) < 0) {
+    /* ERROR HANDLING; you can check errno to see what went wrong */
+    printf("ARRRGGGGGGGG ioctl failed\n");
+    return(1);
+  }
+  
+  char buf[10];
+  while(1) {
+    /* Using I2C Read, equivalent of i2c_smbus_read_byte(file) */
+    if (read(file, buf, 1) != 1) {
+      /* ERROR HANDLING: i2c transaction failed */
+      printf("read failed !!!!!!");
+      return(1);
+    } else {
+      /* buf[0] contains the read byte */
+      putc(buf[0], stdin);
+    }
+  }
+  return 0;
+}
